97d26e5d45f6f7f550017a860323b4f5e2719198
AWDGroup5 <125899758+AWDGroup5@users.noreply.github.com>
2023-03-29 12:30:27 +0100

    Revert Pt.4 Installed Apex Charts #Steve

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import VueApexCharts from "vue3-apexcharts";
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(VueApexCharts);
+app.use(ElementPlus)
 
 app.mount('#app')
