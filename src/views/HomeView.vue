307c0e759e971d4a15cab19de054a7354effd0b0
Akingsley98 <20107626@stu.mmu.ac.uk>
2023-04-16 23:10:57 +0100

    I have started working on the home page and i have added a section to show the feeds. tomorrow i will finish the page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
-<script setup>
-</script>
-
 <template>
-  <main>
-  </main>
+<div class="content">
+      <div class="news-feed">
+        <h2>Latest News on Cardiomyopathy</h2>
+        <rss-feed url="https://rss.app/feeds/1mDqPNunGm48692G.xml"></rss-feed>
+      </div>
+</div>
+<div class="posts-feed">
+        <h2>Latest Posts from Our Website</h2>
+        <post-feed></post-feed>
+      </div>
+        <div class="graph-data">
+        <h2>Overall Graph Data</h2>
+        <graph></graph>
+      </div>
 </template>
+<style>
+.news-feed, .posts-feed {
+  background-color: #f8f8f8;
+  padding: 16px;
+border-radius: 8px;
+}
+</style>
