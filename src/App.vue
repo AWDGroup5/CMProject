4873f377ee97125fe00d4d502f149ed16e90a66a
AWDGroup5 <125899758+AWDGroup5@users.noreply.github.com>
2023-05-02 08:25:37 +0100

    Fixed issue with Data screen not working correctly. Uploaded Mureed's style changes to App.vue #Steve

--- src/App.vue
+++ src/App.vue
@@ -9,161 +9,137 @@ const toggleSidebar = () => {
   sidebarOpen.value = !sidebarOpen.value
 }
 </script>
-
 <template>
-  <div class="Layout-app">
-    <div class="Layout-header">
-      <div class="Header-homepage">
-        <div class="Header-menu" @click="toggleSidebar">
+  <div class="app">
+    <header class="header">
+      <div class="headerwrapper">
+        <div class="headermenu" @click="toggleSidebar">
           <label v-if="!sidebarOpen">Menu</label>
           <label v-else>X</label>
         </div>
-        <nav class="Header-login">
+        <nav class="headernav">
           <RouterLink to="/login">Login</RouterLink>
         </nav>
       </div>
-    </div>
-    <div class="Layout-main">
-      <div class="TitleCard">
+    </header>
+    <main class="main">
+      <div class="title-card">
         <div class="wrapper">
           <img alt="CMP logo" class="logo" src="@/assets/heart-attack.png" width="125" height="125" />
           <HelloWorld msg="Cardiomyopathy Project" />
         </div>
       </div>
-      <div class="ContentCard">
+      <div class="content-card">
         <RouterView />
       </div>
-    </div>
-    <div class="NavBar"></div>
-    <div class="Sidebar" v-show="sidebarOpen" :class="{ show: sidebarOpen }">
+    </main>
+    <div class="nav-bar"></div>
+    <div class="sidebar" v-show="sidebarOpen" :class="{ show: sidebarOpen }">
+      <div class="sidebarclose" @click="toggleSidebar">X</div>
       <nav>
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/news">News</RouterLink></li>
-        <li><RouterLink to="/search">Search</RouterLink></li>
-        <li><RouterLink to="/message-board">Message Board</RouterLink></li>
-        <li><RouterLink to="/data">Data</RouterLink></li>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/news">News</RouterLink>
+        <RouterLink to="/search">Search</RouterLink>
+        <RouterLink to="/message-board">Message Board</RouterLink>
+        <RouterLink to="/data">Data</RouterLink>
       </nav>
     </div>
   </div>
 </template>
-
 <style scoped>
-.Layout-main {
-  box-sizing: inherit;
-  width: 1000px;
-  margin-right: auto;
-  margin-left: auto;
-  line-height: 1.5;
-  max-height: 100vh;
+/* Reset some styles */
+html, body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: #0a529e;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.header {
+
+  border-bottom: 1px solid #f70561;
+  padding: 0 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+}
+
+.headerwrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.headermenu {
+  cursor: pointer;
+}
+
+.main {
+  flex: 1;
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 2rem;
+  box-sizing: border-box;
+}
+
+.title-card {
+  margin-bottom: 2rem;
 }
 
 .logo {
   display: block;
   width: 90px;
   height: 80px;
-  overflow: hidden;
-  background-image: url(./assets/heart-attack.png);
-  background-repeat: no-repeat;
-  background-size: 90px 80px;
-  text-indent: -9999px;
+  margin-bottom: 1rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 13px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-.Layout-header,
-.Layout-main {
-  position: relative;
-  width: 100%;
-  margin: 0 auto;
-}
-
-.Layout-header {
-  z-index: 70;
-}
-
-.Header-homepage {
-  position: relative;
-  width: 100%;
-  min-height: 55px;
-}
-
-.Header-menu {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 55px;
-  height: 55px;
-  text-align: right;
-  cursor: pointer;
-  color: red;
-}
-
-.Header-login {
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 55px;
-  height: 55px;
-  text-align: center;
-}
-
-.Sidebar {
+.sidebar {
   position: fixed;
   left: 0;
   top: 0;
   width: 250px;
   height: 100%;
-  background-color: rgb(242, 240, 240);
+  background-color: #eb0d4c;
   overflow-y: auto;
   padding: 1rem;
+  z-index: 100;
+  transition: transform 0.3s;
+  transform: translateX(-100%);
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.sidebar.show {
+  transform: translateX(0);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.sidebar nav {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.sidebar nav a {
+  padding: 0.5rem 1rem;
+  background-color: #070707;
+  border-radius: 4px;
+}
+.sidebar__close {
+  position: absolute;
+  top: 0px;
+  right: 2px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1280px) {
-  .Layout-main {
-    display: block;
-    position: relative;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  .Layout-main .wrapper {
-    display: block;
-    place-items: start;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import { ref } from "vue";
 const options = [
 {
     value: 'Male',
